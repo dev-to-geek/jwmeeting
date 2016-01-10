9c6e53172570ad54a73721a10946dcaa108d2656
Mircha Emanuel `ryuujin` D'Angelo <mirchaemanuel@gmail.com>
2016-01-11 00:13:38 +0100

    commit frontend project structure

---
+++ JWMeeting/src/org/dev2geek/jwmeeting/fe/Main.java
@@ -0,0 +1,21 @@
+package org.dev2geek.jwmeeting.fe;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Class: Main
+ *
+ * @author Mircha Emanuel `ryuujin` D'Angelo
+ * @version 1
+ */
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ JWMeeting/src/org/dev2geek/jwmeeting/fe/controllers/MainController.java
@@ -0,0 +1,11 @@
+package org.dev2geek.jwmeeting.fe.controllers;
+
+
+/**
+ * Class: MainController
+ *
+ * @author Mircha Emanuel `ryuujin` D'Angelo
+ * @version 1
+ */
+public class MainController {
+}
