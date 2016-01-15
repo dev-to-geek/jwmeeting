bec7dd9bb04cf7fde3c6ab58a51e9c7fdcc96cee
Mircha Emanuel `ryuujin` D'Angelo <mirchaemanuel@gmail.com>
2016-01-15 20:01:39 +0100

    merged

---
+++ JWMeeting/src/main/java/org/dev2geek/jwmeeting/fe/controllers/base/schedule/MeetingScheduleController.java
@@ -0,0 +1,12 @@
+package org.dev2geek.jwmeeting.fe.controllers.base.schedule;
+
+/**
+ * Class: MeetingScheduleController
+ *
+ * @author Mircha Emanuel `ryuujin` D'Angelo
+ * @version 1
+ */
+public class MeetingScheduleController {
+}
+
+    
