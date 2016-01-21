e0e55c2ad8d770dd2eb2473145453469f1a2ce6e
Mircha Emanuel `ryuujin` D'Angelo <mirchaemanuel@gmail.com>
2016-01-21 01:01:00 +0100

    added Unit Tests -> MeetingScheduleBuilderTest

---
+++ JWMeeting/src/test/java/org/dev2geek/jwmeeting/base/schedule/builder/MeetingScheduleBuilderTest.java
@@ -0,0 +1,309 @@
+/*******************************************************************************
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This file is part of JWMeeting.
+ * <p>
+ * JWMeeting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * JWMeeting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  This file is part of JWMeeting.
+ *  <p>
+ *  JWMeeting is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *   <p>
+ *  JWMeeting is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *   <p>
+ *  You should have received a copy of the GNU General Public License
+ *  along with JWMeeting.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.dev2geek.jwmeeting.base.schedule.builder;
+
+import org.dev2geek.jwmeeting.base.schedule.MeetingSchedule;
+import org.dev2geek.jwmeeting.base.schedule.chunks.MeetingChunk;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by MirchaEmanuel on 21/01/2016.
+ */
+public class MeetingScheduleBuilderTest {
+    private MeetingScheduleBuilder meetingScheduleBuilder;
+
+    @Before
+    public void setUp() throws Exception {
+        initNewBuilder();
+    }
+
+
+    private void initNewBuilder() {
+        meetingScheduleBuilder = new MeetingScheduleBuilder();
+    }
+
+    @Test
+    public void testIsValid() {
+        meetingScheduleBuilder.setTime(LocalTime.now());
+        assertFalse(meetingScheduleBuilder.isValid());
+        meetingScheduleBuilder.setDate(LocalDate.now());
+        assertFalse(meetingScheduleBuilder.isValid());
+
+        //check new MeetingScheduleBuilder
+        initNewBuilder();
+        meetingScheduleBuilder.setDateTime(LocalDateTime.now());
+        assertFalse(meetingScheduleBuilder.isValid());
+
+        //check with Date and Time and MeetingChunk is valid
+        meetingScheduleBuilder.addMeetingChunk(mock(MeetingChunk.class));
+        assertTrue(meetingScheduleBuilder.isValid());
+
+        //check without Date and Time, but with MeetingChunk is not valid
+        initNewBuilder();
+        meetingScheduleBuilder.addMeetingChunk(mock(MeetingChunk.class));
+        assertFalse(meetingScheduleBuilder.isValid());
+    }
+
+    @Test
+    public void testIsDateNull() {
+        assertTrue(meetingScheduleBuilder.isDateNull());
+        meetingScheduleBuilder.setDate(LocalDate.now());
+        assertFalse(meetingScheduleBuilder.isDateNull());
+
+        initNewBuilder();
+        meetingScheduleBuilder.setTime(LocalTime.now());
+        assertTrue(meetingScheduleBuilder.isDateNull());
+        meetingScheduleBuilder.setDateTime(LocalDateTime.now());
+        assertFalse(meetingScheduleBuilder.isDateNull());
+    }
+
+    @Test
+    public void testIsTimeNull() {
+        assertTrue(meetingScheduleBuilder.isTimeNull());
+        meetingScheduleBuilder.setTime(LocalTime.now());
+        assertFalse(meetingScheduleBuilder.isTimeNull());
+
+        initNewBuilder();
+        meetingScheduleBuilder.setDate(LocalDate.now());
+        assertTrue(meetingScheduleBuilder.isTimeNull());
+        meetingScheduleBuilder.setDateTime(LocalDateTime.now());
+        assertFalse(meetingScheduleBuilder.isTimeNull());
+    }
+
+    @Test
+    public void isMeetingChunksEmpty() {
+        assertTrue(meetingScheduleBuilder.isMeetingChunksEmpty());
+        meetingScheduleBuilder.addMeetingChunk(mock(MeetingChunk.class));
+        assertFalse(meetingScheduleBuilder.isMeetingChunksEmpty());
+    }
+
+    @Test
+    public void createIfNotValidThrowsException() {
+        try {
+            meetingScheduleBuilder.create();
+            fail("should throw excpeption");
+        } catch (MeetingScheduleBuilderStateException e) {
+            //success
+
+        }
+    }
+
+    @Test
+    public void createOptional() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Optional<MeetingSchedule> optional = meetingScheduleBuilder.createOptional();
+        assertFalse(optional.isPresent());
+        meetingScheduleBuilder.setDateTime(localDateTime);
+        meetingScheduleBuilder.addMeetingChunk(mock(MeetingChunk.class));
+        optional = meetingScheduleBuilder.createOptional();
+        assertTrue(optional.isPresent());
+        assertEquals(optional.get().getDate(), localDateTime.toLocalDate());
+        assertEquals(optional.get().getStartTime(), localDateTime.toLocalTime());
+    }
+
+    @Test
+    public void create() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        meetingScheduleBuilder.setDateTime(localDateTime);
+        meetingScheduleBuilder.addMeetingChunk(mock(MeetingChunk.class));
+        try {
+            MeetingSchedule meetingSchedule = meetingScheduleBuilder.create();
+            assertNotNull(meetingSchedule);
+            assertEquals(meetingSchedule.getDate(), localDateTime.toLocalDate());
+            assertEquals(meetingSchedule.getStartTime(), localDateTime.toLocalTime());
+        } catch (MeetingScheduleBuilderStateException e) {
+            fail();
+        }
+    }
+
+}
